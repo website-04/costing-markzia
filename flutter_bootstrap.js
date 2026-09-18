d5041aa6e7517aa3c78373568e305b2b14c29f63
website-04 <siteweb004@gmail.com>
2026-09-18 19:33:37 +0000

    Deploying to gh-pages from @ website-04/costing-markzia@c86f574cd5bfd622e106eda42cc46b110c2b37db 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"06a2e2a110089dff50fe635cffd2a61e1b24fb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2568533882" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1125046703" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
